d23ed40d645904aacb59a439b5b0d14294a2d295
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-07-28 20:11:22 +0200

    feat: solve iteration 1

---
+++ src/iteration-1.js
@@ -0,0 +1,15 @@
+// Iteration 1: All directors? - Get the array of all directors.
+// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
+// How could you "clean" a bit this array and make it unified (without duplicates)?
+function getAllDirectors(moviesArray) {
+  let directors = moviesArray.map((movie) => movie.director);
+  let filteredDirectors = [];
+  for (director of directors) {
+    if (!filteredDirectors.includes(director)) {
+      filteredDirectors.push(director);
+    }
+  }
+  return filteredDirectors;
+}
+
+console.log(getAllDirectors(movies));
