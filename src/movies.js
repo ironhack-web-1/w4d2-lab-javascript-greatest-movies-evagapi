dd0d0b0c40bce9d6eb8aedd350995dd96b19f676
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-07-28 22:08:52 +0200

    feat: solve iteration 4

--- src/movies.js
+++ src/movies.js
@@ -1,6 +1,3 @@
-// Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
-
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
 
