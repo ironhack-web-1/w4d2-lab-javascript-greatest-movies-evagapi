679c51e01d02434fbab53cccaac657327b0568e5
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-07-28 20:25:48 +0200

    feat: solve iteration 2

--- src/movies.js
+++ src/movies.js
@@ -1,8 +1,3 @@
-
-
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
-
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {}
 
