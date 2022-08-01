8758005e195348a9837090667f24d23b1e661cb3
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-08-02 00:26:41 +0200

    feat: solve iteration 7 (bonus)

--- src/movies.js
+++ src/movies.js
@@ -1,5 +1,2 @@
-// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
-
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {}
